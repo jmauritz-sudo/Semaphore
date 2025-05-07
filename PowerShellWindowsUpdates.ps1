7aa1716388ec007ddc1a4464e21ddbda3fbdc48f
jmauritz-sudo <jmauritz@heartland.farm>
2025-05-07 14:11:54 -0500

    Update PowerShellWindowsUpdates.ps1

--- PowerShellWindowsUpdates.ps1
+++ PowerShellWindowsUpdates.ps1
@@ -1,8 +1,1 @@
-# Make sure the execution policy is set to All Signed
-Set-ExecutionPolicy -ExecutionPolicy AllSigned
 
-Write-Output "Checking to see if the Windows Update cmdlets are installed"
-Install-Module PSWindowsUpdate -Confirm
-
-Write-Output "Installing Windows Updates"
-Get-WindowsUpdate -AcceptAll -Install -AutoReboot
